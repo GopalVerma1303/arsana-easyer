c5df20516704c846a73e39709b54ecff47c92ff3
Mansi Kalra <118623172+mansi104-ai@users.noreply.github.com>
2025-02-17 11:19:42 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       <h1 className="text-5xl font-extrabold text-gray-900 mb-4">Welcome to Arsana Labs!!</h1>
       <h2 className="text-2xl font-medium text-gray-700 max-w-2xl">
         We are Arsana Labs – we build open-source products and provide expert software consulting.
+        
       </h2>
     </div>
   );
