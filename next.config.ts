862cc1e08e04d376768d4e5e6e94dd29163ee860
mansi104-ai <mkb.kalra@gmail.com>
2025-03-05 14:01:40 +0530

    trying build

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  output: "export",
+  // output: "export",
   // output: 'export',
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
@@ -12,19 +12,22 @@ const nextConfig = {
     appDir: true,
   },
 }
-//   async redirects() {
-//     return [
-//       {
-//         source: "/",
-//         destination: "/index.html",
-//         permanent: false,
-//       },
-//     ];
-//   },
-// }
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/index.html",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 
 // if (process.env.NODE_ENV === 'development') {
 //   await setupDevPlatform();
 // }
 
 module.exports = nextConfig;
+
+// function redirects() {
+//   throw new Error('Function not implemented.');
+// }
