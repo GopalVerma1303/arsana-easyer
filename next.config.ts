fa7e276bc9a57e9c206d6b8142f5bd22ad3c8fb7
mansi104-ai <mkb.kalra@gmail.com>
2025-03-05 13:46:44 +0530

    files updated-v3

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,31 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  output: "standalone",
   // output: 'export',
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
   },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/index.html",
-        permanent: false,
-      },
-    ];
+  experimental: {
+    appDir: true,
   },
-}
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/index.html",
+//         permanent: false,
+//       },
+//     ];
+//   },
+// }
 
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   await setupDevPlatform();
+// }
+env: {
+  DATABASE_URL: process.env.DATABASE_URL,
+},
 
-module.exports = nextConfig;
+module.exports = nextConfig
